6c3e7c69a23301b875c9e948e8b8f29810097671
themindstorm <parth.kabra@gmail.com>
2020-04-22 01:48:30 +0800

    added IDs

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ for word in text.split(" "):
     # find the blank type
     blank_type = word.split("__")[1]
 
-    blanks_dict[blank_type] = []
-    blanks_counter[blank_type] += 1
+    # check if it's an IDed blank
+    if "/" in blank_type:
+      blanks_dict[blank_type] = ""
+    else:
+      blanks_dict[blank_type] = []
+      blanks_counter[blank_type] += 1
 
 print(blanks_dict)
 print(blanks_counter)
@@ -26,24 +30,41 @@ shuffle(keys)
 print(keys)
 
 for blank in keys:
-  # no of the blanks required
-  no_required = blanks_counter[blank]
-
-  print(blank, no_required)
-  for i in range(0, no_required):
-    # asking user to fill the blank
+  # first check if it's an IDed blank
+  if "/" in blank:
     fill = input(f"{blank}: ")
-    blanks_dict[blank].append(fill)
-  
+    # since there is only one possible fill for Ided, no array here
+    blanks_dict[blank] = fill
+  else:
+    # no of the blanks required
+    no_required = blanks_counter[blank]
+
+    print(blank, no_required)
+    for i in range(0, no_required):
+      # asking user to fill the blank
+      fill = input(f"{blank}: ")
+      blanks_dict[blank].append(fill)
+
 print(blanks_dict)
 
 
 # filling in the blanks with what the user entered
 new_text = ""
+
 for word in text.split(" "):
+  suffix = ""  # this is punctuation (if any)
+  
   if "__" in word:
     blank_type = word.split("__")[1]
-    word = blanks_dict[blank_type].pop()
-  new_text += f"{word} "
+
+    # checking if there's a punctuation
+    try: suffix = word.split("__")[2]
+    except: pass
+
+    if "/" in blank_type:
+      word = blanks_dict[blank_type]
+    else:
+      word = blanks_dict[blank_type].pop()
+  new_text += f"{word}{suffix} "
 
 print(new_text)
